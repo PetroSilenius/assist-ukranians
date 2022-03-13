2a77ea3ebc267715be82108d7258eb504dbac86b
Petro Silenius <petro.silenius@gmail.com>
2022-03-13 19:17:24 +0200

    Update cause listing to match design UI

--- components/CauseList.tsx
+++ components/CauseList.tsx
@@ -1,7 +1,7 @@
-import { useState } from "react";
+import { ChangeEvent, ChangeEventHandler, useState } from "react";
 import CauseCard from "components/CauseCard";
 import { useTranslation } from "hooks/useTranslation";
-import { doc, onSnapshot } from "firebase/firestore";
+import { doc } from "firebase/firestore";
 import { useDocumentData } from "react-firebase-hooks/firestore";
 import db from "firestore";
 
@@ -11,50 +11,64 @@ export const CauseList = <T extends { [key: string]: Cause[] }>({
   reactions,
 }: {
   causes: T;
-  type: "help" | "seek";
+  type: "give" | "seek";
   reactions: number[];
 }) => {
   const t = useTranslation();
 
   type Category = keyof typeof causes;
   const categories = Object.keys(causes) as Category[];
-  const [selectedCategory, setSelectedCategory] = useState<Category>(
-    categories[0]
-  );
+  const [selectedCategories, setSelectedCategories] =
+    useState<Category[]>(categories);
+
+  const handleChecboxChange = (event: ChangeEvent) => {
+    const { name } = event.target as HTMLInputElement;
+    if (selectedCategories.includes(name)) {
+      setSelectedCategories(selectedCategories.filter((c) => c !== name));
+    } else {
+      setSelectedCategories([...selectedCategories, name]);
+    }
+  };
 
   const documentRef = doc(
     db,
     "reactions",
-    type === "help" ? "giveCauses" : "seekCauses"
+    type === "give" ? "giveCauses" : "seekCauses"
   );
   const [value, loading] = useDocumentData(documentRef);
 
   // Handmade prefetching
   const reactionValues = loading ? reactions : value;
 
-  const categoryCauses = causes[selectedCategory].sort(
+  let categoryCauses: Cause[] = [];
+  selectedCategories.forEach((category) => {
+    categoryCauses.push(...causes[category]);
+  });
+
+  categoryCauses.sort(
     (a, b) => reactionValues?.[b.nameId] - reactionValues?.[a.nameId]
   );
 
   return (
     <div className="flex flex-col w-full max-w-3xl">
-      <div className="flex">
+      <div className="flex justify-evenly sticky top-0 bg-primary-dark p-4 z-10">
         {categories.map((category) => {
           const categoryAsString = category as string;
-          const checked = categoryAsString === selectedCategory;
+          const checked = selectedCategories.includes(categoryAsString);
           return (
             <label
               key={categoryAsString}
-              className={`p-3 rounded-lg mr-2 cursor-pointer ${
-                checked ? "bg-yellow-400" : "bg-yellow-300"
+              className={`px-3 py-2 rounded-full cursor-pointer border-2 border-white-400 ${
+                !checked && "opacity-60"
               }`}
             >
               <input
-                type="radio"
+                type="checkbox"
                 hidden
                 key={categoryAsString}
+                name={categoryAsString}
                 checked={checked}
-                onChange={() => setSelectedCategory(categoryAsString)}
+                onChange={handleChecboxChange}
               />
               {t[categoryAsString]}
             </label>
@@ -65,7 +79,6 @@ export const CauseList = <T extends { [key: string]: Cause[] }>({
         <CauseCard
           key={cause.nameId}
           cause={cause}
-          type={type}
           reactionsAmount={reactionValues?.[cause.nameId]}
           documentRef={documentRef}
         />
